3d7aedfc9cae1dc537ad0d812d1d0fcd0c63dd2d
modularAxiom <45411610+modularAxiom@users.noreply.github.com>
2024-11-15 21:38:04 -0500

    Update connectdb.php

--- connectdb.php
+++ connectdb.php
@@ -3,8 +3,8 @@
     // wherever it is included
     $dbhost = "localhost";
     $dbuser= "root";
-    $dbpass = "cs3319";
-    $dbname = "assign2db";
+    $dbpass = "********";
+    $dbname = "sitedb";
     $connection = mysqli_connect($dbhost, $dbuser,$dbpass,$dbname);
     if (mysqli_connect_errno()) {
         die("database connection failed :" . mysqli_connect_error() . "(" . mysqli_connect_errno() . ")");
